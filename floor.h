06e38dc8dd73749e498307c04502a402e48a94c5
ckykevin <61327517+ckykevin@users.noreply.github.com>
2020-05-01 20:51:45 +0800

    Update and rename level.h to floor.h

---
+++ floor.h
@@ -0,0 +1,10 @@
+#ifndef FLOOR_H
+#define FLOOR_H
+
+int floor_1_main(int* player_status, char* user_name);
+int floor_2_main(int* player_status, char* user_name);
+int floor_3_main(int* player_status, char* user_name);
+int floor_4_main(int* player_status, char* user_name);
+
+#endif
+
--- level.h
+++
@@ -1,9 +0,0 @@
-#ifndef LEVEL_H
-#define LEVEL_H
-
-int floor_1_main(int* player_status[9], char* user_name);
-int floor_2_main(int* player_status[9], char* user_name);
-int floor_3_main(int* player_status[9], char* user_name);
-int floor_4_main(int* player_status[9], char* user_name);
-
-#endif
