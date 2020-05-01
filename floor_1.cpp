58acb57ab40b7264843280d4c946fffae358c0d1
ckykevin <61327517+ckykevin@users.noreply.github.com>
2020-05-01 20:52:34 +0800

    Update and rename level_1.cpp to floor_1.cpp

---
+++ floor_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "floor.h"
+using namespace std;
+
+int floor_1_main(int *role_attribute, char* user_name){
+  cout << "level 1!" << endl;
+  return 0;
+}
--- level_1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "level.h"
-using namespace std;
-
-void level_1(int player_status[12]){
-  cout << "level 1!" << endl;
-}
