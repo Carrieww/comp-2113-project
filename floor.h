f45e0d8aeb0ba5c3d7ea07fc9146026d9d58d1bc
Carrie Wang <61529499+Carrieww@users.noreply.github.com>
2020-05-05 19:42:56 +0800

    Update floor.h

--- floor.h
+++ floor.h
@@ -1,10 +1,9 @@
 #ifndef FLOOR_H
 #define FLOOR_H
 
-void floor_1_main(int* player_status, std::string user_name);
-void floor_2_main(int* player_status, std::string user_name);
-void floor_3_main(int* player_status, std::string user_name);
-void floor_4_main(int* player_status, std::string user_name);
+void floor_1_main(int* player_status, std::string user_name, bool &GameOver);
+void floor_2_main(int* player_status, std::string user_name, bool &GameOver);
+void floor_3_main(int* player_status, std::string user_name, bool &GameOver);
 
 #endif
 
