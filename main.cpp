c56f20c76aa73f7e0c2746db28df655069bb253c
Carrie Wang <61529499+Carrieww@users.noreply.github.com>
2020-05-06 23:44:32 +0800

    Update main

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 int main(){
   ifstream fin;
-  int player_status[9] = {1,1000,10,10,20,50,10,0,0};
-  // [0]: player's Level
-  // [1]: player's EXP
+  int player_status[10] = {1,1,500,20,10,0,0,0,0,0};
+  // [0]: floor of tower
+  // [1]: player's Level
   // [2]: player's HP
   // [3]: player's ATK
   // [4]: player's DEF
-  // [5]: player's Gold
-  // [6]: player's '@' key
-  // [7]: player's '$' key
-  // [8]: diamond
+  // [5]: player's EXP
+  // [6]: player's GOLD
+  // [7]: player's '^' key
+  // [8]: player's '&' key
+  // [9]: player's diamond
+
   bool GameOver = false;
   string user_name;
   cout << "Please input your name (<10 characters):";
@@ -24,27 +26,24 @@ int main(){
   cout << "a: Start a new game\nb: Continues where you left off? \n('a'/'b'): ";
   cin >> ch;
   if(ch == 'a'){
-    cout << "No records of this user name"<<endl;
+    cout << "No records of this user name."<<endl;
     floor_1_main(player_status,user_name, GameOver);
   }else if (ch == 'b'){
-    string filename = user_name+".txt";
+    string filename = "output/"+user_name+".txt";
     fin.open(filename.c_str());
-    int Floor;
     if (fin.is_open()){
-      fin >> Floor;
       for (int i=0;i<9;i++){
         fin >> player_status[i];
       }
       // call functions of different levels
-      if (Floor == 1){floor_1_main(player_status,user_name, GameOver);}
-      else if (Floor  == 2){floor_2_main(player_status,user_name, GameOver);}
-      else if (Floor  == 3){floor_3_main(player_status,user_name, GameOver);}
-      //else if (Floor == 4){floor_4_main(player_status,user_name);};
+      if (player_status[0] == 1){floor_1_main(player_status,user_name, GameOver);}
+      else if (player_status[0] == 2){floor_2_main(player_status,user_name, GameOver);}
+      else if (player_status[0] == 3){floor_3_main(player_status,user_name, GameOver);}
+      else if (player_status[0] == 4){floor_4_main(player_status,user_name, GameOver);};
       fin.close();
     }else{
-      //floor_1_main(player_status,user_name, GameOver);
+      floor_1_main(player_status,user_name, GameOver);
     }
   }
-
   return 0;
 }
