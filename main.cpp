6f935b94a8210cc9279ab1e45a775bce40d6ea70
Carrie Wang <61529499+Carrieww@users.noreply.github.com>
2020-05-06 12:00:20 +0800

    Update main
    read the floor_level of players, then put the player in that floor

--- main.cpp
+++ main.cpp
@@ -7,18 +7,15 @@ using namespace std;
 int main(){
   ifstream fin;
   int player_status[9] = {1,1000,10,10,20,50,10,0,0};
-  // [0]: level of tower
-  // [1]: x-coordinate
-  // [2]: y-coordinate
-  // [3]: player's Level
-  // [4]: player's EXP
-  // [5]: player's HP
-  // [6]: player's ATK
-  // [7]: player's DEF
-  // [8]: player's Gold
-  // [9]: player's '@' key
-  // [10]: player's '$' key
-  // [11]: player's '%' key
+  // [0]: player's Level
+  // [1]: player's EXP
+  // [2]: player's HP
+  // [3]: player's ATK
+  // [4]: player's DEF
+  // [5]: player's Gold
+  // [6]: player's '@' key
+  // [7]: player's '$' key
+  // [8]: diamond
   bool GameOver = false;
   string user_name;
   cout << "Please input your name (<10 characters):";
@@ -32,19 +29,22 @@ int main(){
   }else if (ch == 'b'){
     string filename = user_name+".txt";
     fin.open(filename.c_str());
+    int Floor;
     if (fin.is_open()){
+      fin >> Floor;
       for (int i=0;i<9;i++){
         fin >> player_status[i];
       }
       // call functions of different levels
-      if (player_status[0] == 1){floor_1_main(player_status,user_name, GameOver);}
-      else if (player_status[0] == 2){floor_2_main(player_status,user_name, GameOver);}
-      else if (player_status[0] == 3){floor_3_main(player_status,user_name, GameOver);}
-      else if (player_status[0] == 4){floor_4_main(player_status,user_name);};
+      if (Floor == 1){floor_1_main(player_status,user_name, GameOver);}
+      else if (Floor  == 2){floor_2_main(player_status,user_name, GameOver);}
+      else if (Floor  == 3){floor_3_main(player_status,user_name, GameOver);}
+      //else if (Floor == 4){floor_4_main(player_status,user_name);};
       fin.close();
     }else{
-      floor_1_main(player_status,user_name, GameOver);
+      //floor_1_main(player_status,user_name, GameOver);
     }
   }
+
   return 0;
 }
