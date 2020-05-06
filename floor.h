d6a11f617c8842c99b8d55212aab635821d2818d
Carrie Wang <61529499+Carrieww@users.noreply.github.com>
2020-05-06 23:48:44 +0800

    Update floor.h

--- floor.h
+++ floor.h
@@ -4,6 +4,7 @@
 void floor_1_main(int* player_status, std::string user_name, bool &GameOver);
 void floor_2_main(int* player_status, std::string user_name, bool &GameOver);
 void floor_3_main(int* player_status, std::string user_name, bool &GameOver);
+void floor_4_main(int* player_status, std::string user_name, bool &GameOver);
 
 #endif
 
